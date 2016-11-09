84d1b38d63b37c70ee52bc2d64873e0fc8ecee18
Guilherme Souza <gs.souza1080@gmail.com>
2016-11-09 02:08:37 -0200

    Create Conexao.cs

---
+++ Conexao.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data.OleDb;
+using System.Data.Sql;
+using System.Windows.Forms;
+
+
+namespace RentCar_Project
+{
+    class Conexao
+    {
+        public SqlConnection conectarBanco()
+        {
+            string strCon = "Data Source=GUILHERME;Initial Catalog=PIMIII;Integrated Security=True";
+            SqlConnection con = new SqlConnection(strCon);
+
+            try
+            {
+                con.Open();
+                MessageBox.Show("Banco de Dados conectado com sucesso!");
+                return con;
+                
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Erro de Conexão com o Banco de Dados! {0} Exception caught. " + e);
+                return con;
+            }
+
+        }
+        
+    }
+}
